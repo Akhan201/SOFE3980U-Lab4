2fe951bf48ee412a8851d70e8446489f0cab541f
Arzika <80362695+Akhan201@users.noreply.github.com>
2023-04-06 19:03:30 -0400

    Add files via upload

--- BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/HelloController.java
+++ BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/HelloController.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@Controller //Defines that it is a MVC Archetecture
 public class HelloController {
 
-	@GetMapping("/hello")
-	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		
-		model.addAttribute("name", name);
-		
-		return "hello";
-	}
+    @GetMapping("/hello")
+    public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+
+        model.addAttribute("name", name);
+
+        return "hello";
+    }
 }
